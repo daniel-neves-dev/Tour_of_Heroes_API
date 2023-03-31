b8442e41cfce2b2355c531692dd7901a4ddac89d
daniel <daniel.oliveira.au@hotmail.com>
2023-03-31 20:45:37 -0300

    test: name (case sensitive,token)

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Hero, type: :model do
+  subject{Hero.new(name: "Chun Li", token: "token5000")}
   context 'validations' do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:token)}
     it {should validate_length_of(:token).is_at_least(5)}
+    it {should validate_uniqueness_of(:name).scoped_to(:token).case_insensitive}
   end
 end
 
