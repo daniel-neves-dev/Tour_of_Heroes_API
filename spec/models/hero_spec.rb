8593594c1b2f3bd6f0a953537cc96f844ddb5d18
daniel <daniel.oliveira.au@hotmail.com>
2023-03-31 20:37:24 -0300

    test: token length

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Hero, type: :model do
   context 'validations' do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:token)}
+    it {should validate_length_of(:token).is_at_least(5)}
   end
 end
 
