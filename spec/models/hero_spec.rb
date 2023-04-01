b06b4b3deb616e2233654fc205ebeee5ef018c14
daniel <daniel.oliveira.au@hotmail.com>
2023-04-01 14:35:32 -0300

    test: name scopes

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe Hero, type: :model do
     it {should validate_length_of(:token).is_at_least(5)}
     it {should validate_uniqueness_of(:name).scoped_to(:token).case_insensitive}
   end
+
+  context 'scopes' do
+    it '.by_token'do
+      expect(Hero.where(token:"token5000").to_sql).to eq(Hero.by_token("token5000").to_sql)
+    end
+
+    it '.search' do
+      expect(Hero.where('LOWER(name) LIKE?', "%chun li%").to_sql).to eq(Hero.search('chun li').to_sql)
+    end
+
+    it 'sorted_by_name' do
+      expect(Hero.order(:name).to_sql).to eq(Hero.sorted_by_name.to_sql)
+    end
+  end
 end
 
